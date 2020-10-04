4b796ad78d2a66d441c9901e00c5557c12b8dc3e
Nikolai Poliarnyi <PolarNick239@gmail.com>
2020-10-04 22:19:01 +0300

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import shutil
+import traceback
 import subprocess
 from pathlib import Path
 
@@ -50,6 +51,7 @@ if __name__ == '__main__':
     task_to_exec = {
         "task01": ["aplusb"],
         "task03": ["mandelbrot", "sum", "max_prefix_sum"],
+        "task04": ["matrix_transpose", "matrix_multiplication"],
     }
 
     pr_ignore_list_filename = root_dir / "input/pr_ignore_list.txt"
@@ -72,73 +74,69 @@ if __name__ == '__main__':
     data = load_json("https://api.github.com/repos/{}/pulls?state=open".format(repository_name))
     print("{} Pull Requests...".format(len(data)))
     for pr in sorted(data, key=lambda x: x['number']):
-        number = pr['number']
-        url = pr['html_url']
-        print("PR#{}".format(number))
-        if number in ignored_prs:
-            print("  ignored!")
-            continue
-        pr_data = load_json(pr['url'])
+        try:
+            number = pr['number']
+            url = pr['html_url']
+            print("PR#{}".format(number))
+            if number in ignored_prs:
+                print("  ignored!")
+                continue
+            pr_data = load_json(pr['url'])
+            title = pr_data['title']
+            print("  {}".format(url))
+            print("  {}".format(title))
+            username = pr_data['user']['login']
+            # print("  User: {}".format(username))
+            head_repo = pr_data['head']['repo']['full_name']
+            head_branch = pr_data['head']['ref']
+            base_branch = pr_data['base']['ref']
+            if head_branch != base_branch:
+                print("  Fail! Branch mismatch!")
+                continue
+            task = head_branch
+            print("  Repo: {}:{}".format(head_repo, head_branch))
+            if task not in task_to_exec:
+                print("    NOTHING TO EXECUTE!")
+                continue
+            user_task_dir = Path(repos_dir.format(username=username, task=task))
+            user_task_dir.mkdir(parents=True, exist_ok=True)
+            print("  Working dir: {}".format(user_task_dir))
+            repo_git_url = pr_data['head']['repo']['git_url']
+            repo_dir = user_task_dir / pr_data['head']['repo']['name']
+            if not repo_dir.exists():
+                repo = git.Repo.clone_from(repo_git_url, repo_dir, branch=task)
+                repo.git.pull()
+                print("    cloned!")
+            else:
+                repo = git.Repo(repo_dir)
+                print("    already cloned!")
+            build_dir = repo_dir / 'build'
+            if build_dir.exists():
+                print("    already build!")
+                continue
 
-        title = pr_data['title']
-        print("  {}".format(url))
-        print("  {}".format(title))
-
-        username = pr_data['user']['login']
-        # print("  User: {}".format(username))
-        head_repo = pr_data['head']['repo']['full_name']
-        head_branch = pr_data['head']['ref']
-        base_branch = pr_data['base']['ref']
-        if head_branch != base_branch:
-            print("  Fail! Branch mismatch!")
-            continue
-
-        task = head_branch
-        print("  Repo: {}:{}".format(head_repo, head_branch))
-
-        user_task_dir = Path(repos_dir.format(username=username, task=task))
-        user_task_dir.mkdir(parents=True, exist_ok=True)
-        print("  Working dir: {}".format(user_task_dir))
-
-        repo_git_url = pr_data['head']['repo']['git_url']
-        repo_dir = user_task_dir / pr_data['head']['repo']['name']
-
-        if not repo_dir.exists():
-            repo = git.Repo.clone_from(repo_git_url, repo_dir, branch=task)
-            repo.git.pull()
-            print("    cloned!")
-        else:
-            repo = git.Repo(repo_dir)
-            print("    already cloned!")
-
-        build_dir = repo_dir / 'build'
-        if build_dir.exists():
-            print("    already build!")
-            continue
-
-            print("    cleaning build...")
-            for x in build_dir.glob("*"):
-                if x.is_file():
-                    x.unlink()
-                else:
-                    shutil.rmtree(x)
-        else:
-            build_dir.mkdir(exist_ok=False)
-
-        print("    building...")
-        os.chdir(build_dir)
-
-        print("      cmake init...")
-        cmake_init_output_path = output_path.format(task=task, username=username, output="cmake_init")
-        cmake_init_output = execute(cmake_exe, *cmake_init_params, output_path=cmake_init_output_path)
-
-        print("      cmake build...")
-        cmake_build_output_path = output_path.format(task=task, username=username, output="cmake_build")
-        cmake_build_output = execute(cmake_exe, "--build", str(build_dir), *cmake_build_params, output_path=cmake_build_output_path)
-
-        print("  executing...")
-        for exec_name in task_to_exec[task]:
-            os.chdir(repo_dir)
-            print("    ./{}.exe...".format(exec_name))
-            exec_output_path = output_path.format(task=task, username=username, output="exe_{}".format(exec_name))
-            execute("build/Release/{}.exe".format(exec_name), output_path=exec_output_path)
+                print("    cleaning build...")
+                for x in build_dir.glob("*"):
+                    if x.is_file():
+                        x.unlink()
+                    else:
+                        shutil.rmtree(x)
+            else:
+                build_dir.mkdir(exist_ok=False)
+            print("    building...")
+            os.chdir(build_dir)
+            print("      cmake init...")
+            cmake_init_output_path = output_path.format(task=task, username=username, output="cmake_init")
+            cmake_init_output = execute(cmake_exe, *cmake_init_params, output_path=cmake_init_output_path)
+            print("      cmake build...")
+            cmake_build_output_path = output_path.format(task=task, username=username, output="cmake_build")
+            cmake_build_output = execute(cmake_exe, "--build", str(build_dir), *cmake_build_params,
+                                         output_path=cmake_build_output_path)
+            print("  executing...")
+            for exec_name in task_to_exec[task]:
+                os.chdir(repo_dir)
+                print("    ./{}.exe...".format(exec_name))
+                exec_output_path = output_path.format(task=task, username=username, output="exe_{}".format(exec_name))
+                execute("build/Release/{}.exe".format(exec_name), output_path=exec_output_path)
+        except Exception as e:
+            print(traceback.format_exc())
