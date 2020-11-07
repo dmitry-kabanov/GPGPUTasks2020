aedaf33340993d1f994e45e807f29beb669a4049
Nikolai Poliarnyi <PolarNick239@gmail.com>
2020-11-07 13:45:04 +0300

    task06: merge

--- src/evaluate.py
+++ src/evaluate.py
@@ -53,6 +53,7 @@ if __name__ == '__main__':
         "task03": ["mandelbrot", "sum", "max_prefix_sum"],
         "task04": ["matrix_transpose", "matrix_multiplication"],
         "task05": ["bitonic", "radix"],
+        "task06": ["merge"],
     }
 
     pr_ignore_list_filename = root_dir / "input/pr_ignore_list.txt"
